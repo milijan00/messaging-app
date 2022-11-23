41ed52cbd375400111781978ebd7d2ee7fac6398
Aleksandar Milijanovic <acathebest00@gmail.com>
2022-11-23 11:19:39 +0100

    Routing enabled.

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 import React from 'react';
-import { createRoot } from 'react-dom/client';
+import { BrowserRouter } from 'react-router-dom';
+import ReactDOM from 'react-dom/client';
 import { Provider } from 'react-redux';
 import { store } from './app/store';
 import App from './App';
@@ -7,12 +8,14 @@ import reportWebVitals from './reportWebVitals';
 import './index.css';
 
 const container = document.getElementById('root');
-const root = createRoot(container);
+const root = ReactDOM.createRoot(container);
 
 root.render(
   <React.StrictMode>
     <Provider store={store}>
+    <BrowserRouter>
       <App />
+    </BrowserRouter>
     </Provider>
   </React.StrictMode>
 );
